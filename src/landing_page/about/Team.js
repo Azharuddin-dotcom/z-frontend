7b9415726dba9455f1675f3bfe68d81333aeb7ba
Mr. Azharuddin <azharquraishi94@gmail.com>
2025-03-16 09:03:35 +0530

    Done

--- src/landing_page/about/Team.js
+++ src/landing_page/about/Team.js
@@ -9,7 +9,7 @@ function Team() {
 
             <div className='row p-3 text-muted' style={{lineHeight: '1.8', fontSize: '1.1rem'}}>
                 <div className='col-6 p-3 text-center'>
-                    <img src='media/images/nithinKamath.jpg' style={{borderRadius: '100%', width: "50%"}} />
+                    <img src='media/images/nithinKamath.jpg' style={{borderRadius: '100%', width: "50%"}} alt='img' />
                     <h4 className='mt-5'>Nithin Kamath</h4>
                     <h6>Founder, CEO</h6>
                 </div>
@@ -23,7 +23,7 @@ function Team() {
                     <p>
                         Playing basketball is his zen.
                     </p>
-                    <p>Connect on <a href='' style={{textDecoration:"none"}}>Homepage</a> / <a href='' style={{textDecoration:"none"}}>TradingQnA</a> / <a href='' style={{textDecoration:"none"}}>Twitter</a> </p>
+                    <p>Connect on <a href='/' style={{textDecoration:"none"}}>Homepage</a> / <a href='/' style={{textDecoration:"none"}}>TradingQnA</a> / <a href='/' style={{textDecoration:"none"}}>Twitter</a> </p>
                 </div>
             </div>
         </div>
